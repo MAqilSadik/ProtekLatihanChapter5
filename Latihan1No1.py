bdc34e73d8e6c0537501abaf96b37f77b6221f4c
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-10-30 20:29:31 +0700

    Create Latihan1No1.py

---
+++ Latihan1No1.py
@@ -0,0 +1,14 @@
+indo = int(input("Nilai Bahasa Indonesia :"))
+
+ipa = int(input("Nilai IPA:"))
+
+matematika = int(input("Nilai MTK:"))
+
+
+print("-------------------------")
+
+if(indo>60 and ipa>60 and matematika>70):
+    print ("LULUS")
+else:
+    print ("Tidak Lulus")
+
