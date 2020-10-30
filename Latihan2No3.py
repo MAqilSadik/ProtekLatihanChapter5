0b5890aaebdb0fe8eebdb2eff581920b5b734702
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-10-30 20:30:00 +0700

    Create Latihan2No3.py

---
+++ Latihan2No3.py
@@ -0,0 +1,10 @@
+i = 1
+j = 0
+k = 0
+while(i <= 100):
+    print(i)
+    i = i + 2
+    j = j + 1
+    k = k + i
+print("banyaknya bil ganjil : " + str(j))
+print("jumlah seluruh bilangan : " + str(k))
